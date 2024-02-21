d0b38654a8a70e6b15f5fc9e4070391488afa633
DeclanGH <declanoonunkwo@gmail.com>
2024-02-21 14:56:05 -0500

    TCP client and server for calculating 1mb message throughput

---
+++ src/client_file/TCPClientOneMB.java
@@ -0,0 +1,101 @@
+/*
+ * @author Declan Onunkwo
+ * Spring 2024
+ */
+package client_file;
+
+import functions.HelperFunctions;
+import org.jetbrains.annotations.*;
+
+import java.io.*;
+import java.net.*;
+
+public class TCPClientOneMB {
+
+    private static final int ONE_MEGABYTE = 1048576; // 1,048,576 bytes
+
+    public static void main(@NotNull String[] args) {
+        String serverHostName = args[0];
+        int serverPortNumber = 26942;
+
+        Socket socket = null;
+
+        try {
+            socket = new Socket(serverHostName,serverPortNumber);
+        } catch (UnknownHostException e) {
+            System.err.println("The host \"" + serverHostName + "\" is unknown.");
+            e.printStackTrace();
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection.");
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        try {
+            DataInputStream input = new DataInputStream(socket.getInputStream());
+            DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+
+            BufferedReader stdIn = new BufferedReader(
+                    new InputStreamReader(System.in)
+            );
+
+            long key = Long.MAX_VALUE - 98765;
+            System.out.println("\n--------------------------------------------------------------");
+
+            int timeout = 5000; // milliseconds
+            socket.setSoTimeout(timeout);
+            int droppedPackets = 0;
+
+            mainLoop:
+            while (true) {
+                String userInput = stdIn.readLine();
+                int inputLength = userInput.getBytes().length;
+                int numOfLoops = ONE_MEGABYTE / inputLength;
+                key = HelperFunctions.generateRandomNumber(key);
+                String encryption = HelperFunctions.encrypt(userInput, key);
+
+                long totalTime = 0;
+                int count = numOfLoops;
+
+                while (count > 0){
+                    long startTime = System.nanoTime();
+                    output.writeUTF(encryption);
+                    output.flush();
+                    if (userInput.equals("exit!")) break mainLoop;
+                    try {
+                        if (input.readUTF().equals("received!")){
+                            long endTime = System.nanoTime();
+                            totalTime += endTime - startTime;
+                            count -=  1;
+                        }
+                    } catch (SocketTimeoutException e) {
+                        System.out.println("Timeout occurred, resending packet...");
+                        droppedPackets += 1;
+                        // goes on to resend the packet without adding time or decrementing count
+                    }
+                }
+
+                double timeInSeconds = totalTime / 1e9;
+                int numberOfBitsInMessage = inputLength * 8;
+                double throughput = numberOfBitsInMessage / timeInSeconds;
+
+                System.out.println("A message of " + inputLength + " bytes was sent " + numOfLoops + " times." );
+                System.out.println(droppedPackets + " dropped packets.");
+                System.out.println("Throughput = " + throughput + " bps");
+                System.out.println("--------------------------------------------------------------");
+            }
+
+            System.out.println("closing connection...");
+            output.close();
+            input.close();
+            stdIn.close();
+            socket.close();
+            System.out.println("GoodBye!");
+
+        } catch (IOException e) {
+            System.err.println("IO failure.");
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/server_file/TCPServerOneMB.java
@@ -0,0 +1,72 @@
+/*
+ * @author Declan Onunkwo
+ * Spring 2024
+ */
+package server_file;
+
+import functions.HelperFunctions;
+
+import java.io.*;
+import java.net.*;
+
+public class TCPServerOneMB {
+
+    static final int PORT = 26942;
+
+    public static void main(String[] args) {
+        ServerSocket serverSocket = null;
+        Socket client = null;
+        InputStream input = null;
+        OutputStream output = null;
+        DataInputStream dataInputStream = null;
+        DataOutputStream dataOutputStream = null;
+
+        try {
+            serverSocket = new ServerSocket(PORT);
+            String encryptedMessage;
+            long key = Long.MAX_VALUE - 98765;
+
+            System.out.println("\n--------------------------------------------------------------");
+
+            client = serverSocket.accept();
+            input = client.getInputStream();
+            output = client.getOutputStream();
+
+            String prevMessage = "";
+
+            while (true) {
+                dataInputStream = new DataInputStream(input);
+                dataOutputStream = new DataOutputStream(output);
+
+                encryptedMessage = dataInputStream.readUTF();
+                dataOutputStream.writeUTF("received!");
+                dataOutputStream.flush();
+
+                key = HelperFunctions.generateRandomNumber(key);
+                String decryptedMessage = HelperFunctions.decrypt(encryptedMessage,key);
+
+                if (!decryptedMessage.equals(prevMessage) && decryptedMessage.length() != prevMessage.length()) {
+                    prevMessage = decryptedMessage;
+                    System.out.println("Client Message: " + decryptedMessage);
+                    System.out.println("--------------------------------------------------------------");
+                }
+            }
+
+        } catch(IOException e) {
+            System.out.println("closing connection...");
+            try {
+                dataOutputStream.close();
+                output.close();
+                dataInputStream.close();
+                input.close();
+                client.close();
+                serverSocket.close();
+            } catch (IOException ex) {
+                System.out.println("IO exception");
+            } catch (NullPointerException ex) {
+                System.out.println("no connection was initially opened...");
+            }
+            System.out.println("GoodBye!");
+        }
+    }
+}
